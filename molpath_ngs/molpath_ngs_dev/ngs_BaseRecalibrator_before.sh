e5b07fef7c5561807efc307153a978d10d07aab4
Stephen Newhouse <stephen.j.newhouse@gmail.com>
2013-09-17 14:50:43 +0100

    update added fastq sam email options from config file to master

---
+++ molpath_ngs/molpath_ngs_dev/ngs_BaseRecalibrator_before.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+#$ -S /bin/bash
+#$ -cwd
+#$ -p -0.99999999999999999999999999999999999999999999999999
+#$ -V
+
+###############################################
+## BaseRecalibrator with default covars
+################################################
+
+sample_name=${1}
+sample_dir=${2}
+sample_temp=${3}
+
+cd ${sample_dir}
+
+${java_1.7}/java  -Xmx${gatk_java_mem}g -Djava.io.tmpdir=${sample_temp} -jar ${ngs_gatk}/GenomeAnalysisTK.jar -T BaseRecalibrator -R ${reference_genome_seq}  \
+-I ${sample_dir}/${sample_name}.novorealn.bam \
+-o ${sample_dir}/${sample_name}.novorealn.recal_data.table \
+-knownSites ${b37_1000G_biallelic_indels} \
+-knownSites ${b37_Mills_Devine_2hit_indels_sites} \
+-knownSites ${b37_1000G_omni2_5} \
+-knownSites ${b37_hapmap_3_3} \
+-knownSites ${b37_dbsnp_132};
+##-nct 1 \
+##-log ${sample_dir}/${sample_name}.novorealn.BaseRecalibrator.log;
+
+#--covariate / -cov ( String[] )
+#One or more covariates to be used in the recalibration. 
+#Can be specified multiple times. 
+#Note that the ReadGroup and QualityScore covariates are required and do not need to be specified. 
+##Also, unless --no_standard_covs is #specified, the Cycle and Context covariates are standard and are included by default.
+#Use the --list argument to see the available covariates.
