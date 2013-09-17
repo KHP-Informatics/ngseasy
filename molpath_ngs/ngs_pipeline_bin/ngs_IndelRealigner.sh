63cbd180693312f0f37ec036d05d1f55b195ef22
sjnewhouse <stephen.newhouse@kcl.ac.uk>
2013-09-17 14:22:36 +0100

    sep 17 2013 update 01 added isilon path to master

--- molpath_ngs/ngs_pipeline_bin/ngs_IndelRealigner.sh
+++ molpath_ngs/ngs_pipeline_bin/ngs_IndelRealigner.sh
@@ -103,7 +103,7 @@ echo ".................................................."
 
 ## run IndelRealigner ##
 
-java -Xmx${gatk_java_mem}g -Djava.io.tmpdir=${sample_temp} -jar ${ngs_gatk}/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference_genome_seq}  \
+${java_1.7}/java  -Xmx${gatk_java_mem}g -Djava.io.tmpdir=${sample_temp} -jar ${ngs_gatk}/GenomeAnalysisTK.jar -T IndelRealigner -R ${reference_genome_seq}  \
 -I ${sample_dir}/${sample_name}.novoraw.bam -o ${sample_dir}/${sample_name}.novorealn.bam -targetIntervals ${sample_dir}/${sample_name}.novoraw.output.intervals \
 -known ${b37_1000G_biallelic_indels} -known ${b37_Mills_Devine_2hit_indels_sites} \
 -model USE_READS -LOD 3 -compress 0;
