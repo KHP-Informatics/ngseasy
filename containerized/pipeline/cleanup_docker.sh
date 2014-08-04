6e8f9634a19b748f020e5b5b7e7e8d50e2fc4666
Stephen J Newhouse <stephen.j.newhouse@gmail.com>
2014-08-04 11:30:14 +0100

    var-anno v1 added Dockerfile

--- containerized/pipeline/cleanup_docker.sh
+++ containerized/pipeline/cleanup_docker.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-docker ps -a | grep 'Exit' | awk '{print }' | xargs -r docker rm;
- docker images | grep none | awk '{print }' | xargs -r docker rmi
+
+docker ps -a | grep 'Exit' | awk '{print }' | xargs -r docker rm
+
+docker images | grep none | awk '{print }' | xargs -r docker rmi
