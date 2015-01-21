a309c60aff2c6d16d0b2d79737b6f19eca21a6c9
Stephen Newhouse <stephen.j.newhouse@gmail.com>
2015-01-21 15:04:25 +0000

    base image updates

--- scratch/run_docker.sh
+++ scratch/run_docker.sh
@@ -3,8 +3,9 @@
 # playing and blah
 
 docker run --rm -it \
--v $(HOME)/ngs_projects:/home/pipeman \
+-v ${HOME}/ngs_projects:/home/pipeman/ngs_projects: \
 -w /home/pipeman \
 -e HOME=/home/pipeman \
 -e USER=pipeman \
+--name ngseasy_base \
 --user=pipeman compbio/ngseasy-base:1.0
