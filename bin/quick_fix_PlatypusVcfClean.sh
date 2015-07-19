71e69ce77ccdb7fab0be959bcf1257434e10e074
Stephen Newhouse <stephen.j.newhouse@gmail.com>
2015-07-19 10:13:34 +0100

    dev updates Sun 19 Jul 2015 10:13:34 BST

--- bin/quick_fix_PlatypusVcfClean.sh
+++ bin/quick_fix_PlatypusVcfClean.sh
@@ -2,33 +2,74 @@
 
 
 #################################################################################
+# usage
+#################################################################################
+
+function usage {
+    echo "
+Program: VcfClean
+Version 1.0-r001
+Author: Stephen Newhouse (stephen.j.newhouse@gmail.com)
+
+usage:   VcfClean -v <VCF> -f <Variant Caller>
+
+options:  -v  STRING  configuration file
+          -f  STRING  project directory
+          -h  NULL    show this message
+"
+}
+
+#################################################################################
 # get options
 #################################################################################
 
-VCF_INPUT="/media/Data/ngs_projects/GCAT_Data/NA12878/vcf/platypus_180715/small.vcf"
+  while  getopts "hv:f:" opt
+  do
 
-VCF=`basname ${VCF}`
+      case ${opt} in
+    h)
+    usage #print help
+    exit 0
+    ;;
+
+    c)
+    VCF_INPUT=${OPTARG}
+    ;;
+
+    d)
+    VCF_FORMAT=${OPTARG}
+    ;;
+      esac
+  done
+
+
+VCF_FORMAT="platypus"
+VCF_INPUT="/media/Data/ngs_projects/GCAT_Data/NA12878/vcf/platypus_180715/small.vcf"
 
 #################################################################################
 ## make compatible with other tools and mathc GATK/Freebayes annotaions
 #################################################################################
 
-if [[ gzip ]]
+if [[ ${VCF_FORMAT} == "platypus" ]]
     then
+    if [[ gzip ]]
+        then
         VCF=`basname ${VCF} .gz`
         zcat ${VCF}.gz | sed s/TC\=/DP\=/g | \
         sed s/"GT:GL:GOF:GQ:NR:NV"/"GT:GL:GOF:GQ:DP:NV"/g | bgzip -c > ${VCF}.fix.vcf.gz && \
         tabix ${VCF}.fix.vcf.gz
-elif [[ not gzip ]]
-    then
+    elif [[ not gzip ]]
+        then
         VCF=`basname ${VCF} .vcf`
         cat ${VCF}.vcf | sed s/TC\=/DP\=/g | \
         sed s/"GT:GL:GOF:GQ:NR:NV"/"GT:GL:GOF:GQ:DP:NV"/g |  bgzip -c > ${VCF}.fix.vcf.gz && \
         tabix ${VCF}.fix.vcf.gz
+    else
+        echo "ok"
+    fi
 else
-    ok
+    echo "ok"
 fi
-
 
 ## TO FIX:- Get GATK/Freebayes equivalents and check GEMINI requirements
 ##FORMAT=<ID=NV,Number=.,Type=Integer,Description="Number of reads containing variant in this sample">
