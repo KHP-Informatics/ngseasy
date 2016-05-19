e4047d8c93c165bf676d183754d45bedac84531e
snewhouse <stephen.j.newhouse@gmail.com>
2016-05-19 15:27:34 +0100

    dd

--- Dockerfiles/native_install.sh
+++ Dockerfiles/native_install.sh
@@ -1,45 +1,54 @@
-#!/bin/env bash
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
 
-  INSTALL_DIR="/home/${USER}"
+## version
+VERSION="0.1"
 
-  echo "Installing to [${INSTALL_DIR}]"
-  cd ${INSTALL_DIR} && pwd
+INSTALL_DIR="/home/${USER}"
 
-  wget https://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh && \
-  /bin/bash ./Anaconda2-4.0.0-Linux-x86_64.sh -b -p ${INSTALL_DIR}/anaconda2 && \
-  rm -v ./Anaconda2-4.0.0-Linux-x86_64.sh
-  echo "Anaconda is being installed at [${INSTALL_DIR}/anaconda2]"
+echo "Installing to [${INSTALL_DIR}]"
 
-  # add conda bin to path
-  export PATH=$PATH:${INSTALL_DIR}/anaconda2/bin
-  echo 'PATH=$PATH:${INSTALL_DIR}/anaconda2/bin' >> ~/home/${USER}/.bashrc
+cd ${INSTALL_DIR} && pwd
 
-  # setup conda
-  conda update -y conda
-  conda update -y conda-build
-  conda update -y --all
-  mkdir -p ${INSTALL_DIR}/anaconda2/conda-bld/linux-64 ${INSTALL_DIR}/anaconda2/conda-bld/osx-64
-  conda index ${INSTALL_DIR}/anaconda2/conda-bld/linux-64 ${INSTALL_DIR}/anaconda2/conda-bld/osx-64
+wget https://repo.continuum.io/archive/Anaconda2-4.0.0-Linux-x86_64.sh && \
+/bin/bash ./Anaconda2-4.0.0-Linux-x86_64.sh -b -p ${INSTALL_DIR}/anaconda2 && \
+rm -v ./Anaconda2-4.0.0-Linux-x86_64.sh
+echo "Anaconda is being installed at [${INSTALL_DIR}/anaconda2]"
 
-  ## add channels
-  conda config --add channels bioconda
-  conda config --add channels r
-  conda config --add channels sjnewhouse
+# add conda bin to path
+export PATH=$PATH:${INSTALL_DIR}/anaconda2/bin
+echo 'PATH=$PATH:${INSTALL_DIR}/anaconda2/bin' >> ~/home/${USER}/.bashrc
 
-  ## create ngseasy environment python >=2.7 for ngs tools
-  conda create --yes --name ngseasy
-  source activate ngseasy
+# setup conda
+conda update -y conda
+conda update -y conda-build
+conda update -y --all
+mkdir -p ${INSTALL_DIR}/anaconda2/conda-bld/linux-64 ${INSTALL_DIR}/anaconda2/conda-bld/osx-64
+conda index ${INSTALL_DIR}/anaconda2/conda-bld/linux-64 ${INSTALL_DIR}/anaconda2/conda-bld/osx-64
 
-  ## ngs tools
-  wget https://github.com/KHP-Informatics/ngseasy/blob/f1000_dev/Dockerfiles/ngs_conda_tool_list.txt
-  conda install -y \
-  --update-dependencies \
-  --file ./ngs_conda_tool_list.txt
-  rm -v ./ngs_conda_tool_list.txt
+## add channels
+conda config --add channels bioconda
+conda config --add channels r
+conda config --add channels sjnewhouse
 
-  ## update nextflow
-  nextflow self-update
+## create ngseasy environment python >=2.7 for ngs tools
+conda create --yes --name ngseasy
+source activate ngseasy
 
-  # list
-  TIME_STAMP=`date +"%d-%m-%y"`
-  conda list -e > ${INSTALL_DIR}/anaconda2/ngseasy-spec-file-${TIME_STAMP}.txt
+## ngs tools
+wget https://github.com/KHP-Informatics/ngseasy/blob/f1000_dev/Dockerfiles/ngs_conda_tool_list.txt
+
+conda install -y \
+--update-dependencies \
+--file ./ngs_conda_tool_list.txt
+rm -v ./ngs_conda_tool_list.txt
+
+## update nextflow
+nextflow self-update
+
+# list
+TIME_STAMP=`date +"%d-%m-%y"`
+conda list -e > ${INSTALL_DIR}/anaconda2/ngseasy-spec-file-${TIME_STAMP}.txt
